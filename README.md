f7f1040de482fbf3fd05e7a715612f71ebe46eaf
Luis Meyer <luis_meyer@outlook.de>
2023-03-20 17:43:16 +0100

    ci: optimize setup script

---
+++ README.md
@@ -0,0 +1,13 @@
+# Pusher 💪
+
+Push-Notifications for everything. Ever wished a Webpage had Push-Notifications? With Pusher you can create Flows that will navigate a Browser through a Website. With a decision tree you can than create messages based on the page content and send them through a push notification channel of your choice.
+
+## Development
+
+1. Make sure [direnv](https://direnv.net/) is installed
+2. Create a .envrc file based on the .envrc.example file
+
+```ts
+npm install
+npm run setup
+```
--- scripts/setup.mjs
+++ scripts/setup.mjs
@@ -3,7 +3,6 @@ import decompress from "decompress";
 import decompressTarxz from "decompress-tarxz";
 import decompressUnzip from "decompress-unzip";
 import { existsSync, writeFileSync } from "fs";
-import { resolve } from "path";
 import fetch from "node-fetch";
 
 const { BUCKET_NAME, IS_LOCAL, LINUX_FFMPEG_URL, MAC_FFMPEG_URL } = process.env;
@@ -16,54 +15,71 @@ if (!MAC_FFMPEG_URL) {
   throw new Error("Missing MAC_FFMPEG_URL");
 }
 
-const getUrl = () => {
-  const linuxUrl = LINUX_FFMPEG_URL;
-  const macUrl = MAC_FFMPEG_URL;
+const url = IS_LOCAL ? MAC_FFMPEG_URL : LINUX_FFMPEG_URL;
 
-  return IS_LOCAL ? macUrl : linuxUrl;
-};
+const client = new S3Client({
+  region: "eu-central-1",
+});
+
+const s3Key = "ffmpeg";
 
 const uploadToS3 = async (data) => {
-  const client = new S3Client({
-    region: "eu-central-1",
-  });
+  console.info("Uploading ffmpeg to S3");
 
   return client.send(
     new PutObjectCommand({
       Bucket: BUCKET_NAME,
-      Key: "ffmpeg",
+      Key: s3Key,
       Body: Buffer.from(data),
     })
   );
 };
 
-const fetchArchive = async () => {
-  const url = getUrl();
+const existsInS3 = async () => {
+  console.info("Checking if ffmpeg exists in S3");
 
+  try {
+    await client.send(
+      new HeadObjectCommand({
+        Bucket: BUCKET_NAME,
+        Key: s3Key,
+      })
+    );
+
+    return true;
+  } catch (e) {
+    return false;
+  }
+};
+
+const fetchArchive = async () => {
   console.info("Downloading ffmpeg archive from " + url);
 
-  const filename = url.split("/").pop();
-
-  if (!filename) {
+  const archiveFilename = url.split("/").pop();
+  if (!archiveFilename) {
     throw new Error("Missing filename");
   }
 
-  const path = resolve("/tmp", filename);
+  const archivePath = "/tmp/" + archiveFilename;
 
-  console.info("Saving ffmpeg archive to " + path);
-
-  if (!existsSync(path)) {
-    await fetch(url)
-      .then((res) => res.arrayBuffer())
-      .then((buffer) => writeFileSync(path, Buffer.from(buffer)));
-
-    console.info("Downloaded ffmpeg archive into " + path);
+  if (existsSync(archivePath)) {
+    console.log("Archive already exists");
+    return archivePath;
   }
 
-  decompress(path, "/tmp", {
-    plugins: [decompressTarxz(), decompressUnzip()],
-  }).then(async (files) => {
-    console.log("decompress done!");
+  return fetch(url)
+    .then((res) => res.arrayBuffer())
+    .then((data) => writeFileSync(archivePath, Buffer.from(data)))
+    .then(() => archivePath);
+};
+
+const decompressArchive = async (archivePath) => {
+  const plugins = [decompressTarxz(), decompressUnzip()];
+
+  console.info("Decompressing ffmpeg archive");
+
+  return decompress(archivePath, "/tmp", { plugins }).then(async (files) => {
+    console.log("Decompress done!");
 
     const file = files.find(({ path }) => path.endsWith("ffmpeg"));
 
@@ -71,12 +87,56 @@ const fetchArchive = async () => {
       throw new Error("Missing ffmpeg file");
     }
 
-    if (file && !IS_LOCAL && BUCKET_NAME) {
-      console.info("Uploading ffmpeg to S3");
-
-      await uploadToS3(file.data);
-    }
+    return file;
   });
 };
 
-fetchArchive();
+const loadFFMPEG = async () => fetchArchive().then(decompressArchive);
+
+const runCi = async () => {
+  if (!BUCKET_NAME) {
+    throw new Error("Missing BUCKET_NAME");
+  }
+
+  const exists = await existsInS3();
+  if (BUCKET_NAME && exists) {
+    return;
+  }
+
+  const file = await loadFFMPEG();
+
+  await uploadToS3(file.data);
+
+  console.info("Done! ffmpeg file uploaded to S3");
+};
+
+const runLocal = async () => {
+  const path = "/tmp/ffmpeg";
+
+  if (existsSync(path)) {
+    console.log("ffmpeg File already exists");
+    return;
+  }
+
+  const file = await loadFFMPEG();
+
+  writeFileSync("/tmp/ffmpeg", file.data);
+
+  console.info("Done! ffmpeg file written to /tmp/ffmpeg");
+};
+
+const main = async () => {
+  if (IS_LOCAL) {
+    console.info("Running local setup...");
+
+    runLocal();
+  }
+
+  if (!IS_LOCAL) {
+    console.info("Running CI setup...");
+
+    runCi();
+  }
+};
+
+main();
